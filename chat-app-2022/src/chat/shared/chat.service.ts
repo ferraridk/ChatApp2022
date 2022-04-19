a15bc52dc7af91b0850cdd721d5659c25f6dc6b8
Henrik <henrikferrari@gmail.com>
2022-04-19 11:36:27 +0200

    a bit of this and that and an interface.

---
+++ chat-app-2022/src/chat/shared/chat.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import {ChatClient} from "./chat-client.model";
+
+@Injectable()
+export class ChatService {
+    allMessages: string[] = [];
+    clients: ChatClient[] = [];
+
+    addMessage(message: string): void {
+        this.allMessages.push(message);
+    }
+
+    addClient(id: string, nickname: string): void {
+        this.clients.push({id: id, nickname: nickname});
+    }
+
+    getClients(): ChatClient[] {
+        return this.clients;
+    }
+
+    getMessages(): string[] {
+        return this.allMessages;
+    }
+
+    deleteClient(id: string): void {
+        this.clients = this.clients.filter((c) => c.id !== id);
+    }
+}
